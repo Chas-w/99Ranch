359450c9dfba1b53db16d8c2ead82059074c468b
Chase Dodge <75660257+Chas-w@users.noreply.github.com>
2025-03-12 22:31:26 -0400

    Trying to set up dialogue system that stores words so that I can eventually click and translate them

---
+++ Assets/Scripts/Translatable.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Translatable : MonoBehaviour
+{
+    public string[] translatableInSentence; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/WordSpawn.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using Unity.VisualScripting;
+
+public class WordSpawn : MonoBehaviour
+{
+    [Header("Word Data")]
+    [SerializeField] TextMeshProUGUI textMeshObject;
+    [SerializeField] int spawnSpeedMax;
+    [SerializeField] int currentMessage;
+    public bool nextMessage;
+    public bool triggerText = false; 
+
+
+    [Header("Message Contents")]
+    [SerializeField] string[] message;
+    [SerializeField] string[] messageDeconstructed;
+
+    string displayMessage = "";
+    float spawnSpeed;
+    int i = 0;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        spawnSpeed = spawnSpeedMax;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        messageDeconstructed = message[currentMessage].Split(' ');
+
+       
+
+        if (Input.GetMouseButtonDown(0)) //cycle through dialogue
+        {
+            currentMessage++;
+            i = 0; 
+            nextMessage = false;
+            displayMessage = ""; 
+        }
+
+        ShowMessage(); 
+
+        textMeshObject.text = displayMessage;
+
+
+    }
+
+
+    private void ShowMessage()
+    {
+
+        do
+        {
+
+            if (messageDeconstructed[i] != null)
+            {
+                displayMessage += messageDeconstructed[i] + " ";
+                i++;
+                Debug.Log(i);
+            }
+
+        }
+        while (i <= messageDeconstructed.Length - 1);
+
+   
+
+    }
+
+}
